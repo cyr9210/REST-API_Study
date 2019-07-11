b2e78b8fdc333978d82d7954e4035a4690f616bc
cyr9210 <jipsyer@naver.com>
2019-07-11 11:16:06 +0900

    입력값 제한하기

--- 02/src/main/java/me/bong/springrestapi/events/EventController.java
+++ 02/src/main/java/me/bong/springrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package me.bong.springrestapi.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,9 +19,12 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+//        Event event = Event.builder().build(); // 이런식으러 넣어줘야한다.
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
--- 02/src/test/java/me/bong/springrestapi/events/EventControllerTest.java
+++ 02/src/test/java/me/bong/springrestapi/events/EventControllerTest.java
@@ -1,11 +1,14 @@
 package me.bong.springrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -20,21 +23,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(EventController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Autowired
-    ObjectMapper objectMapper;
-
-    @MockBean
-    EventRepository eventRepository;
+    private ObjectMapper objectMapper;
 
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
+                .id(100)
                 .name("spring")
                 .description("rest api")
                 .beginEnrollmentDateTime(LocalDateTime.of(2019, 07, 11, 23, 00))
@@ -45,10 +47,12 @@ public class EventControllerTest {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남")
+                .free(true)
+                .offline(false)
                 .build();
-        event.setId(10);
+//        event.setId(10);
 
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -59,6 +63,8 @@ public class EventControllerTest {
                  .andExpect(jsonPath("id").exists())
                  .andExpect(header().exists(HttpHeaders.LOCATION))
                  .andExpect(header().stringValues(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                 .andExpect(jsonPath("id").value(Matchers.not(100)))
+                 .andExpect(jsonPath("free").value(Matchers.not(true)))
          ;
     }
 }
