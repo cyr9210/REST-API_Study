381a3f599fe759ac74db7d10f81abb731bc87da5
cyr9210 <jipsyer@naver.com>
2019-07-16 22:24:24 +0900

    예외발생

--- 02/src/main/java/me/bong/springrestapi/events/EventController.java
+++ 02/src/main/java/me/bong/springrestapi/events/EventController.java
@@ -5,10 +5,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -20,10 +22,18 @@ public class EventController {
 
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
 //        Event event = Event.builder().build(); // 이런식으러 넣어줘야한다.
+        if (errors.hasErrors())
+            return ResponseEntity.badRequest().build();
+
+        eventValidator.validate(eventDto, errors);
+        if (errors.hasErrors())
+            return ResponseEntity.badRequest().build();
+
         Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- 02/src/test/java/me/bong/springrestapi/events/EventControllerTest.java
+++ 02/src/test/java/me/bong/springrestapi/events/EventControllerTest.java
@@ -1,6 +1,7 @@
 package me.bong.springrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.bong.springrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -66,5 +67,69 @@ public class EventControllerTest {
                  .andExpect(jsonPath("id").value(Matchers.not(100)))
                  .andExpect(jsonPath("free").value(Matchers.not(true)))
          ;
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("spring")
+                .description("rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 07, 11, 23, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 07, 12, 00, 00))
+                .beginEventDateTime(LocalDateTime.of(2019, 07, 13, 00, 00))
+                .endEventDateTime(LocalDateTime.of(2019, 07, 14, 00, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남")
+                .free(true)
+                .offline(false)
+                .build();
+//        event.setId(10);
+
+//        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+        ;
+    }
+
+    @Test
+    public void createEvent_BadRequest_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest())
+        ;
+    }
+
+    @Test
+    @TestDescription("입력받을수 없는 입력값을 사용한 경우에 에러를 발생시킨다.")
+    public void createEvent_BadRequest_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("spring")
+                .description("rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 07, 12, 23, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 07, 11, 00, 00))
+                .beginEventDateTime(LocalDateTime.of(2019, 07, 14, 00, 00))
+                .endEventDateTime(LocalDateTime.of(2019, 07, 13, 00, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest())
+        ;
     }
 }
